9928010d9e2ac539fc79dc55e3f2acd8636206cf
Praveen Jayakody <3007446+praveenjayakody@users.noreply.github.com>
2025-06-03 10:15:23 +0530

    Initialize fetching-and-context

---
+++ src/app/(client)/login/layout.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from "react"
+
+export default function LoginLayout ({
+  children
+}: {
+  children: ReactNode
+}): React.ReactNode {
+  return (
+    <div id='login-layout'>{children}</div>
+  )
+}
