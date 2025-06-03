fb4df1210df4155ca2428527bdd17d64b76e2b6d
devzx3 <devzxthree@gmail.com>
2025-06-03 17:53:15 +0530

    Update ForgotPasswordForm.tsx

--- src/app/(client)/[lng]/forgot-password/partials/ForgotPasswordForm.tsx
+++ src/app/(client)/[lng]/forgot-password/partials/ForgotPasswordForm.tsx
@@ -3,49 +3,98 @@
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
+
+// Forgot Password email validations schema
+const ForgotPasswordschema = z.object({
+  email: z.string().nonempty("Required").email("Please enter a valid email.")
+})
+
+type ForgotPasswordFormData = z.infer<typeof ForgotPasswordschema>
 
 export default function ForgotPasswordForm(): React.ReactNode {
   const router = useRouter()
 
+  // validate form
+  const form = useForm<ForgotPasswordFormData>({
+    resolver: zodResolver(ForgotPasswordschema),
+    defaultValues: {
+      email: ""
+    }
+  })
+
+  // form Submit function
+  const onSubmit = async (data: ForgotPasswordFormData): Promise<void> => {
+    const formData = new FormData()
+    formData.append("email", data.email)
+
+    console.log("verify email Submitted:", data)
+
+    router.push(`/forgot-password/otp?email=${data.email}`)
+  }
+
   return (
-    <form className="overflow-y-auto md:grid">
-      {/* second column */}
-      <div className="flex flex-col items-center justify-center max-w-[450px] mx-auto px-5 space-y-3 md:space-y-4 py-5">
-        <div className="space-y-2">
-          {/* title  */}
-          <h1 className="text-xl font-semibold text-center md:text-2xl">
-            Forgot Password
-          </h1>
-          <div className="px-5 text-center">
-            <Label className=" text-Primary-300">
-              Please enter your email address to receive a verification code.
-            </Label>
+    <Form {...form}>
+      <form
+        className="overflow-y-auto md:grid"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        {/* second column */}
+        <div className="flex flex-col items-center justify-center max-w-[450px] mx-auto px-5 space-y-3 md:space-y-4 py-5">
+          <div className="space-y-2">
+            {/* title  */}
+            <h1 className="font-semibold text-center ">Forgot Password</h1>
+
+            <div className="px-5 text-center">
+              <Label className=" text-Primary-300">
+                Please enter your email address to receive a verification code.
+              </Label>
+            </div>
           </div>
+
+          {/* email */}
+          <div className="w-[100%]">
+            {/* Email Field */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          {/* login button  */}
+          <Button className="w-[100%]" type="submit">
+            Send OTP
+          </Button>
+
+          {/* back button  */}
+          <Link href={"/login"}>
+            <Button className="px-0 py-0" type="button" variant={"link"}>
+              Back
+            </Button>
+          </Link>
         </div>
-
-        {/* email */}
-        <div className="w-[100%]">
-          <Label className="w-[100%]">Email</Label>
-          <Input placeholder={"Email"} />
-        </div>
-
-        {/* login button  */}
-        <Button className="w-[100%]" type="submit">
-          Send OTP
-        </Button>
-
-        <Button
-          className="px-0 py-0"
-          type="button"
-          variant={"link"}
-          onClick={() => {
-            router.push("/login")
-          }}
-        >
-          Back
-        </Button>
-      </div>
-    </form>
+      </form>
+    </Form>
   )
 }
