15171aeb9283ff582f313450372c347d918d3079
devzx3 <devzxthree@gmail.com>
2025-06-05 16:46:44 +0530

    delete layout, loading, not found

--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-// loading.tsx
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <p className="text-xl animate-pulse">Loading...</p>
-    </div>
-  )
-}
--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-// not-found.tsx
-export default function NotFound() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">404 – Page Not Found</h1>
-    </div>
-  )
-}
