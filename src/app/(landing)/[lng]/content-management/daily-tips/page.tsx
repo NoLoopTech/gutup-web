ce26845ce8f9dc30ce1bb2b6268dc7f03bc68921
devzx3 <devzxthree@gmail.com>
2025-06-11 12:49:42 +0530

    add daily tip and mood popup

--- src/app/(landing)/[lng]/content-management/daily-tips/page.tsx
+++ src/app/(landing)/[lng]/content-management/daily-tips/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/nextAuthOptions"
 import { redirect } from "next/navigation"
 import DailyTipsPage from "../partials/DailyTipsPage"
+import { Button } from "@/components/ui/button"
 
 export default async function DailyTips(): Promise<any> {
   const session = await getServerSession(authOptions)
@@ -10,5 +11,12 @@ export default async function DailyTips(): Promise<any> {
     redirect("/login")
   }
 
-  return <DailyTipsPage />
+  return (
+    <main>
+      <div className="flex justify-end mb-5 -mt-14">
+        <Button>Add New</Button>
+      </div>
+      <DailyTipsPage />
+    </main>
+  )
 }
--- src/app/(landing)/[lng]/content-management/moods/page.tsx
+++ src/app/(landing)/[lng]/content-management/moods/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/nextAuthOptions"
 import { redirect } from "next/navigation"
 import MoodsPage from "../partials/MoodsPage"
+import { Button } from "@/components/ui/button"
 
 export default async function Moods(): Promise<any> {
   const session = await getServerSession(authOptions)
@@ -10,5 +11,12 @@ export default async function Moods(): Promise<any> {
     redirect("/login")
   }
 
-  return <MoodsPage />
+  return (
+    <main>
+      <div className="flex justify-end mb-5 -mt-14">
+        <Button>Add New</Button>
+      </div>
+      <MoodsPage />
+    </main>
+  )
 }
